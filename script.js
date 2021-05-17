6a49c97b6c9c6a4a8b3db24df8b71ed5cf9052ac
Wassim <wassimrached404@gmail.com>
2021-05-17 23:07:17 +0100

    add background,some js work ,data sign in , navbar fix problems ,and more....

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
-window.addEventListener("scroll",function () {
-  const header = document.querySelector('header');
+// LOAD
+
+let loading = document.getElementById('lds-ring');
+window.addEventListener('load',function () {
+    loading.style.display = "none"
+})
+
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
-  var navHome = document.getElementById('navHome')
-  var navContact = document.getElementById('navContact')
-  var navAbout = document.getElementById('navAbout')
-  var navLearn = document.getElementById('navLearn')
-  navHome.classList.toggle('active', window.scrollY >= 0 && window.scrollY < 381)
-  navContact.classList.toggle('active', window.scrollY > 382 && window.scrollY < 1275)
-  navAbout.classList.toggle('active', window.scrollY > 1276 && window.scrollY < 2213)
-  navLearn.classList.toggle('active', window.scrollY > 2214 )
+  var navHome = document.getElementById("navHome");
+  var navContact = document.getElementById("navContact");
+  var navAbout = document.getElementById("navAbout");
+  var navLearn = document.getElementById("navLearn");
+  navHome.classList.toggle(
+    "active",
+    window.scrollY >= 0 && window.scrollY < 381
+  );
+  navContact.classList.toggle(
+    "active",
+    window.scrollY > 382 && window.scrollY < 1275
+  );
+  navAbout.classList.toggle(
+    "active",
+    window.scrollY > 1276 && window.scrollY < 2213
+  );
+  navLearn.classList.toggle("active", window.scrollY > 2214);
 });
 
 
 var navPhone = document.getElementById("ULphone");
 function showNavPhone() {
-  navPhone.classList.toggle('show')
-  document.body.classList.toggle('NoScrol')
+  navPhone.classList.toggle("show");
+  document.body.classList.toggle("NoScrol");
+  document.getElementById('humberger').classList.toggle('close')
 }
-
 
 function displayMenu() {
-  var menu = document.getElementById('menu')
-  menu.classList.toggle('left0')
-  if (menu.classList.contains('left0')) {
-    document.getElementById('settingsIcon').className = "fas fa-angle-left"
-  }else{
-    document.getElementById('settingsIcon').className = "fas fa-angle-right"
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("left0");
+  if (menu.classList.contains("left0")) {
+    document.getElementById("settingsIcon").className = "fas fa-angle-left";
+  } else {
+    document.getElementById("settingsIcon").className = "fas fa-angle-right";
   }
 }
-// // LANGUAGE CHANGE 
-// var arabicBtn = document.getElementById('arabic')
-// var englishBtn = document.getElementById('english')
 
-// arabicBtn.addEventListener('click',function changeLanguage() {
-//   var section1 =  document.querySelectorAll('section1')
-//   section1.
-// })
-// englishBtn.addEventListener('click',function changeLanguage() {
-//   var section1 =  document.querySelectorAll('section1')
-//   section1[0].innerHTML = "english" 
-// })
+// LANGUAGE CHANGE
+/*
+var arabicBtn = document.getElementById('arabic')
+var englishBtn = document.getElementById('english')
+
+arabicBtn.addEventListener('click',function changeLanguage() {
+  var section1 =  document.querySelectorAll('section1')
+})
+
+englishBtn.addEventListener('click',function changeLanguage() {
+  var section1 =  document.querySelectorAll('section1')
+  section1[0].innerHTML = "english"
+})
+*/
---
+++ sign.js
@@ -0,0 +1,71 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+    checkIfSignInSucceed();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'this is Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue.length < 8) {
+		setErrorFor(password, 'your password must be at least 8 characters');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value.length < 8) {
+		setErrorFor(password2, 'your password must be at least 8 characters');
+	} else if(passwordValue !== password2Value ) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function checkIfSignInSucceed(){
+    if (document.querySelectorAll('.success').length == 4) {
+        document.getElementById('signInSucceed').style.display = "flex"
+    }
+
+}
